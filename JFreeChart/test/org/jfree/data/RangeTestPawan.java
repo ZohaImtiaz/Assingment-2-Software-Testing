7f04a0a002d10693c5e6d4329ae8f06f37c997bb
Pawan Tiwana <113376274+ptiwa918@users.noreply.github.com>
2026-02-11 17:36:49 -0700

    Update RangeTestPawan.java

--- JFreeChart/test/org/jfree/data/RangeTestPawan.java
+++ JFreeChart/test/org/jfree/data/RangeTestPawan.java
@@ -1,1 +1,59 @@
+package org.jfree.data;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RangeTestPawan {
+
+    private Range range1;
+    private Range range2;
+
+    @BeforeEach
+    void setup() {
+      range1 = new Range(2, 6);
+      range2 = new Range(4, 8);
+    }
+
+
+  
+  // equals() Tests
+
+  @Test
+  void testEqualsSameValues(){
+    Range sameRange = new Range(2, 6);
+    assertTrue(range1.equals(sameRange));
+  }
+
+  
+  @Test
+  void testEqualsDifferentLower() {
+    Range different = new Range(3, 6);
+    assertFalse(range1.equals(different));
+  }
+
+  
+  @Test
+  void testEqualsDifferentUpeer() {
+    Range different = new Range(2, 7);
+    assertFalse(range1.equals(different));
+  }
+
+  
+  @Test
+  void testEqualsNull() {
+    assertFalse(range1.equals(null));
+  }
+
+
+  @Test
+  void testEqualsDifferentObjectType() {
+    assertFalse(range1.equals("NotARange"));
+  }
+
+  
+
+  
+
+  
